d9c8d41728ab43cfbb91f4bea635eb8ccd88ad62
Micha Glas <migl3228@gmail.com>
2023-03-23 13:06:24 +0100

    new routing

---
+++ ChatApp/Controllers/ChatsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ChatApp.Controllers
+{
+    public class ChatsController : Controller
+    {
+        //GET: Chats
+        public IActionResult List()
+        {
+            return View();
+        }
+
+        public IActionResult Chat()
+        {
+            return View();
+        }
+    }
+}
