e8f0f232385d2cda0a813195cf6f3525baf7078d
Micha Glas <migl3228@gmail.com>
2023-03-29 10:54:23 +0200

    added chats

---
+++ ChatApp/Models/Chat.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ChatApp.Models
+{
+    public class Chat
+    {
+        [Key]
+        private int id { get; set; }
+        private string name { get; set; }
+        private string description { get; set; }
+        private string genre { get; set; }
+        private string? user_id { get; set; }
+        
+    }
+}
---
+++ ChatApp/Models/ChatMessage.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ChatApp.Models
+{
+    public class ChatMessage
+    {
+        [Key]
+        private int id { get; set; }
+        private int chat_id { get; set; }
+        private string message { get; set; }
+        private string? user_id { get; set; }
+    }
+}
