36922bf6aaed282e5054367353f979804c76894c
Micha Glas <migl3228@gmail.com>
2023-04-16 21:09:57 +0200

    load messages from database

--- ChatApp/wwwroot/js/Component.js
+++ ChatApp/wwwroot/js/Component.js
@@ -4,13 +4,7 @@ import { SenderMessage } from "/js/components/senderMessage.js";
 
 //attach templates
 template.attachTemplates();
-var message = Message;
-
-//create components
-const messageComponent = new Message("eerste", "17:09", "user1");
-const senderComponent = new SenderMessage("tweede", "17:09")
-
-//append components
 const chat = document.getElementById("chat");
-chat.prepend(senderComponent);
-chat.prepend(messageComponent);
+myArray.forEach((element) => {
+    chat.appendChild(new Message(element.message, element.time, element.userId));
+});
