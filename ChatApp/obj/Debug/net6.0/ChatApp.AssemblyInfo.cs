9b11a3b1b8434f0982f0fceb619b9d19cf20c47a
Micha Glas <migl3228@gmail.com>
2024-01-23 15:31:04 +0100

    gitignore update
    f

--- ChatApp/obj/Debug/net6.0/ChatApp.AssemblyInfo.cs
+++ ChatApp/obj/Debug/net6.0/ChatApp.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChatApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e6a0f543d75ca3299a7aaeb74cf9fbc9a1eb205")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChatApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChatApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
